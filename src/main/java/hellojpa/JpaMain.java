1207d5e1e1f34709dad2f6506127fb447fa76340
조용일 <yedn1128@naver.com>
2022-03-05 18:32:13 +0900

    기본키 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,12 +18,24 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setId(1L);
-            member.setUsername("A");
-            member.setRoleType(RoleType.USER);
+            Member member1 = new Member();
+            member1.setUsername("A");
 
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("B");
+
+            Member member3 = new Member();
+            member3.setUsername("C");
+
+            System.out.println("===========================");
+
+            em.persist(member1);    // 1, 51로 맞춤
+            em.persist(member2);    // Memory 호출
+            em.persist(member3);    // Memory 호출
+
+            System.out.println("member1 = " + member1.getId());
+            System.out.println("member2 = " + member2.getId());
+            System.out.println("member3 = " + member3.getId());
 
             System.out.println("===========================");
 
